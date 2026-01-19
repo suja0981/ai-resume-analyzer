0986a6c69ee3ce7d68bc79e6f216752ce33bbf38
[Sujal Wadhankar] <[sujalwadhankar938@gmail.com]>
2026-01-19 22:56:28 +0530

    Added Homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants";
+import ResumCard from "~/components/ResumeCard";
+import { resume } from "react-dom/server.node";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Generates smart feedback for resumes" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <main className="bg-[url('/images/bg-main.svg')] bg cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review Your Submissions and check AI-powered feedback </h2>
+      </div>
+
+
+      {resume.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumCard key={resume.id} resume={resume} />
+          ))}
+
+        </div>
+
+      )}
+    </section>
+
+  </main>
 }
