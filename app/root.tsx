7ad5f2dd71d521d068c1c9504835faf28207b9dc
[Sujal Wadhankar] <[sujalwadhankar938@gmail.com]>
2026-01-20 00:44:51 +0530

    Authentication

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
-
+import { useEffect } from "react";
+import { usePuterStore } from "~/lib/puter";
 import type { Route } from "./+types/root";
 import "./app.css";
 
@@ -24,6 +25,10 @@ export const links: Route.LinksFunction = () => [
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const{init}= usePuterStore();
+
+  useEffect(() => {
+    init()}, [init]);
   return (
     <html lang="en">
       <head>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,9 @@ import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
 import ResumCard from "~/components/ResumeCard";
 import { resume } from "react-dom/server.node";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({ }: Route.MetaArgs) {
   return [
@@ -12,6 +15,13 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+   const {auth} = usePuterStore();
+   const navigate=useNavigate();
+
+   useEffect(() => {
+if (!auth.isAuthenticated) navigate("/auth?next=/");
+   }, [auth.isAuthenticated]);
+
   return <main className="bg-[url('/images/bg-main.svg')] bg cover">
     <Navbar />
 
